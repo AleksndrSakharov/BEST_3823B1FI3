e614669012f79108061036e21f1896ce601a9aef
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2023-10-06 16:18:47 +0300

    Init

--- 2-if_else_switch/Chocolate/chocolate.h
+++ 2-if_else_switch/Chocolate/chocolate.h
@@ -1,3 +1,12 @@
 int chocolate(int n, int m, int k){
-    return 0;
+    if (n > 0 && m > 0){
+       if(n * m == k) return 1;
+    else if ((n*m-k*m)%m==0 ||(n*m-k*n)%n==0){
+        if (((n*m % 2 == 0) && (k % 2 == 0)) || ((n*m % 2 == 1) && (k % 2 == 1))) return 1;
+        else return 0;  
+        }
+    else if (n == 0 || m == 0) return 0;
+    return 0; 
+    }
+    return 0; 
 }
