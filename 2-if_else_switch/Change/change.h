594447478ad7864444a0a570a0fa1b48aba1bd4f
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2023-10-06 15:28:23 +0300

    Init

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,3 +1,6 @@
 long long change(int a, int b, int c, int d){
-    return 0;
+    long long sd;
+    sd = ((long long)c * 100 + (long long)d) - ((long long)a * 100 + (long long)b);
+    if (sd >= 0) return sd;
+    return -1;
 }
