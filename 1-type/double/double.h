4f96dd3188bab8fff41e90919dc5fd3ba05178cf
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-10-22 18:08:55 +0300

    Init

--- 1-type/double/double.h
+++ 1-type/double/double.h
@@ -1,11 +1,25 @@
+#include <math.h>
+// double r_value(double val, int acc){
+// int divider = 1;
+// for(int i = 0; i < acc; i++){
+//     val *= 10;
+//     divider *= 10;
+// }
+// return (round(val) / divider);
+// }
+
 bool compare_double(double x, double y) {
-    return true; // Not implementation
+    return (fabs(x - y) < 0.00001);
 }
 
 int get_nearest_int(double x) {
-    return 0; // Not implementation
+    double res;
+    res = round(x);
+    return res;
 }
 
 double get_fractional(double x) {
-    return 0.1; // Not implementation
+    int integer_part = (int)x;
+    if(x > 0) return x - integer_part;
+    return x - integer_part + 1;
 }
