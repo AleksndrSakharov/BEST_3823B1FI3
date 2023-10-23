c9081da0e770923aa2cc88ca9afa97905ccf0be3
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-10-23 11:02:06 +0300

    why

--- 1-type/char-category/char-category.h
+++ 1-type/char-category/char-category.h
@@ -1,24 +1,45 @@
+// #include <stdbool.h>
+
+
+// bool is_digit(char x) {
+//     if (x >= 48 && x <= 57) return true;
+//     return false; // No implementation
+// }
+
+// bool is_letter(char x) {
+//     if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122)) return true;
+//     return false; // No implementation
+// }
+
+// bool is_punctuation(char x) {
+//     if ((x >= 33 && x <= 47) || (x >= 58 && x <= 64) || (x >= 91 && x <= 96) || (x >= 123 && x <= 126)) return true;
+//     return false; // No implementation
+// }
+
+// int get_ascii_code(char first, char second, char third) {
+//     int result = (first * 1000000) + (second * 1000) + third;
+//     return result; // No implementation
+    
+// }
 #include <stdbool.h>
 
 
-bool is_digit(char x) {
-    if (x >= 48 && x <= 57) return true;
-    return false; // No implementation
+bool is_digit(char x) 
+{
+    return (x >= 48 && x <= 57) ? true : false;
 }
 
-bool is_letter(char x) {
-    if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122)) return true;
-    return false; // No implementation
+bool is_letter(char x) 
+{
+    return ( (x >= 65 && x <= 90) || (x >= 97 && x <= 122) ) ? true : false;
+}
+   
+bool is_punctuation(char x) 
+{
+    return ( (x >= 33 && x <= 47) || (x >= 58 && x <= 64) || (x >= 91 && x <= 96) || (x >= 123 && x <= 126)  ) ? true : false;
 }
 
-bool is_punctuation(char x) {
-    if ((x >= 33 && x <= 47) || (x >= 58 && x <= 64) || (x >= 91 && x <= 96) || (x >= 123 && x <= 126)) return true;
-    return false; // No implementation
+int get_ascii_code(char first, char second, char third) 
+{
+    return first * 1000000 + second * 1000 + third;
 }
-
-int get_ascii_code(char first, char second, char third) {
-    int result = (first * 1000000) + (second * 1000) + third;
-    return result; // No implementation
-    
-}
-
