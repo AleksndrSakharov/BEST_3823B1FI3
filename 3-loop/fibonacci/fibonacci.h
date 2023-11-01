6a66f55f6c8287a5c8f14677274d38c5fedcfe00
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-11-01 23:19:11 +0300

    213

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,3 +1,18 @@
 long fib(int m){
-    return 0;
+    if(m < 0)
+        return -1;
+    int s = 1;
+    int cur_fib = 1;
+    int last_fib = 1;
+    int temp_value = 0;
+    while(cur_fib <= m)
+    {
+        s += cur_fib;
+        temp_value = cur_fib;
+        cur_fib += last_fib;
+
+        last_fib = temp_value;
+
+    }
+    return s;
 }
--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,5 +1,8 @@
 #include <math.h>
 
+
 char find_digit(double number, char digit){
+    double dig = digit, res, c;
+    int f = 0;
     return 100;
 }
