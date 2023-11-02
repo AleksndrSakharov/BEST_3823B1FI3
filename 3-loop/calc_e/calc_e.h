224fa24eb14068c540216513e212336263a24866
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-11-02 13:19:36 +0300

    123

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -1,6 +1,14 @@
 #include <math.h>
 
 unsigned long long calc_e(char order){
-    return 0;
+    double e = 0;
+    int n = 0, k = 1;
+    if(order == 1){
+        while(e < 2.7){
+            e = pow((1.0 + 1.0 / (double)k), (double)k);
+            k++;
+        }
+    }
     
+    return k;
 }
