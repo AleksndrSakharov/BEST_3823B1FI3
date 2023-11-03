54558c8507f002bb4acf7625975e7c87eab2673f
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2023-11-03 15:21:48 +0300

    123

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -9,6 +9,29 @@ unsigned long long calc_e(char order){
             k++;
         }
     }
-    
+    if(order == 2){
+        while(e < 2.71){
+            e = pow((1.0 + 1.0 / (double)k), (double)k);
+            k++;
+        }
+    }
+    if(order == 3){
+        while(e < 2.718){
+            e = pow((1.0 + 1.0 / (double)k), (double)k);
+            k++;
+        }
+    }
+    if(order == 4){
+        while(e < 2.7182){
+            e = pow((1.0 + 1.0 / (double)k), (double)k);
+            k++;
+        }
+    }
+    if(order == 5){
+        while(e < 2.71828){
+            e = pow((1.0 + 1.0 / (double)k), (double)k);
+            k++;
+        }
+    }
     return k;
 }
