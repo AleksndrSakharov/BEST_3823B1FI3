59eb805b6a9a4aba083df51c107aabd72d479ec4
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2023-11-03 16:00:47 +0300

    qw

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -2,7 +2,7 @@
 
 unsigned long long calc_e(char order){
     double e = 0;
-    int n = 0, k = 1;
+    int k = 0;
     if(order == 1){
         while(e < 2.7){
             e = pow((1.0 + 1.0 / (double)k), (double)k);
