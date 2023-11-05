48606efe8e4358dc1efa369fd5b1494b867acfe7
Андрей Степанов <therain3164@yandex.ru>
2023-11-05 10:59:32 +0300

    123

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -1,7 +1,39 @@
 #include <math.h>
 
 int find_elem(int left, int right, int number, int max_iter){
-    int i = 0; //Счётчик итераций
+    int i = 0, j = 0, l = left, k = right, n = number; //Счётчик итераций
+    while(l != 0){
+        l--;
+        k--;
+        n--;
+    }
+    
+    while(1 > 0){
+        if(k % 2 == 0){
+            k = (k - l) / 2;
+            i++;
+            if(n > k + l){
+                l += k;
+                k = k + l;
+            }
+            if(n == k + l){
+                n = k + l - 1;
+                break;
+            }
+        }
+        else{
+            k = (k - l + 1) / 2;
+            i++;
+            if(n > k + l){
+                l += k;
+                k += l;
+            }
+            if(n == k + l){
+                n = k + l - 1;
+                break;
+            }
+        }
+    }
 
 
     return i; //НЕ МЕНЯТЬ!
