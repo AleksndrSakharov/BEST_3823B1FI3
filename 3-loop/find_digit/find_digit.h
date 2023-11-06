1fcee5db296491924d5465cc9f25815ecf567d1f
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-11-06 11:53:40 +0300

    123

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,50 +1,30 @@
 #include <math.h>
 
-
-int stepen(int n, int st){
-    int res = 1;
-    for(int i = 1; i <= st; i++) res *= n;
-    return res;
-}
-
-int count_float_dig(double acc)
-{
-    int count = 0;
-    int integer = (int)acc;
-    while (integer != acc)
-    {
-        acc *= 10;
-        count++;
-        integer = (int)acc;
-    }
-    return count;
-}
-
-int count_do_tochki(int x){
-    int count = 0;
-    while (x > 0){
-        x /= 10;
-        count++;
-    }
-    return count;
-}
-
 char find_digit(double number, char digit){
-    int count = 0;
-    int c1 = count_do_tochki((int)number);
     if(digit > 9 || digit < 0) return -1;
-    unsigned long long n = stepen(10, count_float_dig(number)) * number;
-    while(n > 0){
-        count++;
-        if(n % 10 != digit){
-            n /= 10;
-        }
-        if(n % 10 == digit){
-            break;
+    long long wholePart, fractionalPart, t1, t2, a[30] = {};
+    wholePart = (long long) number;
+    fractionalPart = (long long)((number - (double)wholePart) * 1000000000);
+    t1 = wholePart;
+    t2 = fractionalPart;
+    char count = 0, i = 0, flag = 0;
+    for(i = 0; i < 30; i++) a[i] = 10;
+    i = 29;
+    while (fractionalPart > 0){
+        a[i--] = fractionalPart % 10;
+        fractionalPart /= 10;
+    }
+    i--;
+    while (wholePart > 0){
+        a[i--] = wholePart % 10;
+        wholePart /= 10;
+    }
+    for(i = 0; i < 30; i++){
+        if(a[i] < 10) flag = 1;
+        if(flag == 1){
+            count++;
+            if(a[i] == digit) return count;
         }
     }
-    if(c1 <= count){
-        count++;
-    }
-    return count;
+    return -1;
 }
