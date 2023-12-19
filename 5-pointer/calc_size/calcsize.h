f9aef84f018b89255b0940fed5dd0c80d8de020c
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-12-19 14:56:02 +0300

    1

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -1,6 +1,19 @@
 #include <math.h>
 
-int calcsize(void* memory){
-    int tipe = sizeof(memory);
-    return tipe;
+int calcsize(void* memory)
+{
+    char* arr = (char*)memory;
+    int size = 0;
+    while (*arr != 47)
+    {
+        arr++;
+        size++;
+    }
+    int c = 0;
+    while (*arr != 11)
+    {
+        arr--;
+        c++;
+    }
+    return (size+c);
 }
