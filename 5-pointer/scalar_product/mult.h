de22fc7f2052df9f2e44f5d53ba6e63c931ece92
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2023-12-21 16:58:56 +0300

    123

--- 5-pointer/scalar_product/mult.h
+++ 5-pointer/scalar_product/mult.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 int scalar_product(int* vec1, int* vec2, int size){
-    return 100;
+    double res = 0;
+    double chisl = 0;
+    double fzn = 0, szn = 0, znam = 0;
+    for (int i = 0; i < size; i++){
+        chisl += vec1[i] * vec2[i];
+        fzn += vec1[i] * vec1[i];
+        szn += vec2[i] * vec2[i];
+    }
+    znam = sqrt(fzn * szn);
+    res = acos(chisl / znam);
+    return (int)(res * 180 / 3.14159265);
 }
